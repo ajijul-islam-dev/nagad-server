b52133adecd49ed36783e60e3d696e5cde2f9046
ajijul-islam-dev <ajijulislam5938@gmail.com>
2024-07-17 00:14:59 -0700

    user aproval added

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.port || 3000;
 
-app.use(cors({
-
-}));
+app.use(cors({}));
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wugjgdu.mongodb.net/?appName=Cluster0`;
 
@@ -27,48 +24,50 @@ const client = new MongoClient(uri, {
 
 const database = client.db("nagad");
 const usersCollection = database.collection("users");
+const TransCollection = database.collection("transactions");
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     //  verifyToken -----------------------------------------------------
-    const verifyToken = (req,res,next)=>{
-        const token = req.headers.token;
-        if(!token){
-            return res.send("fobidden acces")
-        }
+    const verifyToken = (req, res, next) => {
+      const token = req.headers.token;
+      if (!token) {
+        return res.send("fobidden acces");
+      }
 
-        jwt.verify(token, process.env.SECRET_TOKEN,(err,decoded)=>{
-
-        })
-    }
-
-
+      jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {});
+    };
 
     // get user --------------------------------------------------------
-    app.get("/user",async (req,res)=>{
-        const token = req.headers.token;
-        if(!token){
-            return res.send({message : "forbidden access", user : null})
-        }
-        jwt.verify(token, process.env.SECRET_TOKEN,async(err,decoded)=>{
-            const phone = decoded?.phone;
-            const user = await usersCollection.findOne({phone})
-            res.send({user})
-        })
-        
+    app.get("/user", async (req, res) => {
+      const token = req.headers.token;
+      if (!token) {
+        return res.send({ message: "forbidden access", user: null });
+      }
+      jwt.verify(token, process.env.SECRET_TOKEN, async (err, decoded) => {
+        const phone = decoded?.phone;
+        const user = await usersCollection.findOne({ phone });
+        res.send({ user });
+      });
+    });
 
-    })
+    // get All user -----------------------------------------------------
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     // api for saving user to database -----------------------------------
     app.post("/sign-up", async (req, res) => {
       const user = req.body;
-      const exist = await usersCollection.findOne({$or : [{email : user.phone},{phone : user.phone}]});
-      if(exist){
-        return res.send({message : "user already exist", status : 301})
+      const exist = await usersCollection.findOne({
+        $or: [{ email: user.phone }, { phone: user.phone }],
+      });
+      if (exist) {
+        return res.send({ message: "user already exist", status: 301 });
       }
       const hashedPin = bcrypt.hashSync(user.pin, 14);
       const result = await usersCollection.insertOne({
@@ -86,21 +85,78 @@ async function run() {
         $or: [{ email: phone }, { phone: phone }],
       });
       if (!user) {
-        return res.send({ message: "invalid credential",status : 401 });
+        return res.send({ message: "invalid credential", status: 401 });
       }
 
       const isMatch = bcrypt.compareSync(pin, user.pin);
 
       if (!isMatch) {
-        return res.send({ message: "invalid credential",status : 401 });
+        return res.send({ message: "invalid credential", status: 401 });
       }
 
-      const token = jwt.sign({phone : user.phone}, process.env.SECRET_TOKEN,{
-        expiresIn : "1h"
+      const token = jwt.sign({ phone: user.phone }, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
       });
+      res.send({ user, token });
+    });
 
+    // aprov users----------------------------------------------------------
+    app.put("/approve", async (req, res) => {
+      const phone =await req.body.phone;
+      const user = await usersCollection.findOne({ phone });
 
-      res.send({user,token});
+      if (user.role.name === "agent" && user.role.status === "pending") {
+        const updateDoc = {
+          $set: {
+            balance: 10000,
+            "role.status": "approved",
+          },
+        };
+        const result = await usersCollection.updateOne({phone},updateDoc)
+        return res.send({message :"updated", result})
+      }
+
+      if (user.role.name === "user" && user.role.status === "pending") {
+        const updateDoc = {
+          $set: {
+            balance: 40,
+            "role.status": "approved",
+          },
+        };
+        const result = await usersCollection.updateOne({phone},updateDoc)
+        return res.send({message :"updated", result})
+      }
+      if (user.role.name === "admin" && user.role.status === "pending") {
+        const updateDoc = {
+          $set: {
+            "role.status": "approved",
+          },
+        };
+        const result = await usersCollection.updateOne({phone},updateDoc)
+        return res.send({message :"updated", result})
+      }
+
+        console.log(phone)
+        res.send({phone})
+    });
+
+    
+    // Transactions ---------------------------------------------------------
+    app.post("/transactions", async (req, res) => {
+      const info = await req.body;
+      const userPhone = info.userNumber;
+      const pin = info.pin;
+
+      const user = await usersCollection.findOne({ phone: userPhone });
+      const isMatch = bcrypt.compareSync(pin, user.pin);
+
+      if (!isMatch) {
+        return res.send({ message: "invalid credential", status: 301 });
+      }
+
+      const result = await TransCollection.insertOne(info);
+
+      res.send({ message: "Request sent", status: 200 });
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -111,8 +167,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 app.listen(port, () => {
   console.log("Nagad server Running On PORT", port);
